9390ef3f5fa1ac0a44a5709e9653d0f3dd06900b
cpgeier <cpg3rb@virginia.edu>
2018-01-27 13:49:45 -0500

    WOWEEE lots of changes

--- map/static/map/datamap.js
+++ map/static/map/datamap.js
@@ -1,6 +1,17 @@
 var bombMap = new Datamap({
   element: document.getElementById("map"),
-  scope: 'usa',
+  scope: 'world',
+  setProjection: function(element) {
+    var projection = d3.geo.equirectangular()
+      .center([-98.5,40])
+      .rotate([4.4, 0])
+      .scale(400)
+      .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    var path = d3.geo.path()
+      .projection(projection);
+
+    return {path: path, projection: projection};
+  },
   fills: {
     defaultFill: "#FFFFFF",
     win: '#000000',
